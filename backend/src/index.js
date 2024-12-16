61f13e384a2193a7d5eb376dcfb7c0c3330ed0c0
izaan <izaansaquib@gmail.com>
2024-12-17 03:02:23 +0500

    feat: cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,15 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser);
+app.use(cors({
+    origin:"*",
+    credentials: true
+}));
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
