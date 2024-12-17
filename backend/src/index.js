b26325993ca59e6aa1aa4c7d97d898ef9665af2b
izaan <izaansaquib@gmail.com>
2024-12-18 02:28:25 +0500

    fix: cors errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(cors({
-    origin:"*",
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-
 app.get("/",(req,res)=>{
     res.send(`<h1>Server is Running<h1>`)
-})
+});
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
 
 let PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
